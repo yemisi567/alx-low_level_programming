7284560a8c580a38178af8bc7a11be0413577dc0
yemisi567 <51059680+yemisi567@users.noreply.github.com>
2022-03-15 10:32:02 +0100

    nested

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of integer
+ * @n: integer argument
+ * Return: last digit of number
+ */
+
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+		{
+		_putchar(-l + 48);
+	return (-l);
+		}
+	else
+	{
+	_putchar(l + 48);
+	return (l);
+	}
+}

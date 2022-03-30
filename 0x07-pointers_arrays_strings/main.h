68f35148813571ac654b3904d2f34c28615d6acc
yemisi567 <51059680+yemisi567@users.noreply.github.com>
2022-03-30 21:25:34 +0100

    recursion

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,90 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#define NULL 0
 
 /**
- * _putchar - puts character to standard output
- * @c: character to put to standard output
- */
-
-int _putchar(char c);
-
-/**
- * _memset - fill n bytes of memory with a constant value
- * @s: pointer to memory area
- * @b: constant value
- * @n: number of bytes to fill
- * Return: memory area
- */
-
-char *_memset(char *s, char b, unsigned int n);
-
-/**
- * _memcpy - copy n bytes of memory from source to destination
- * @dest: destination
- * @src: source
- * @n: number of bytes
- * Return: pointer to dest
- */
-
-char *_memcpy(char *dest, char *src, unsigned int n);
-
-/**
- * _strchr - locate 1st occurrence of character in string and returns pointer there
+ * _strchr - locate 1st occurrence of char in string and returns pointer there
  * @s: string to search
  * @c: target characer
  * Return: pointer to that character in string
  */
 
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
+{
+	int i = 0;
 
-/**
- * _strspn - return length of string that matches values consistently
- * @s: string to search
- * @accept: target matches
- * Return: number of bytes consecutively matched
- */
+	while (s[i] != '\0' && s[i] != c) /* find match */
+		i++;
 
-unsigned int _strspn(char *s, char *accept);
-
-/**
- * _strpbrk - return pointer to byte in s that matches a byte in accepted target
- * @s: string to search
- * @accept: target matches
- * Return: pointer to index of string at first occurence
- */
-
-char *_strpbrk(char *s, char *accept);
-
-/**
- * _strstr - locate and return pointer to first occurence of substring
- * @haystack: string to search
- * @needle: target substring to search for
- * Return: pointer to index of string at first occurence of whole substring
- */
-
-char *_strstr(char *haystack, char *needle);
-
-/**
- * print_chessboard - print chessboard given set 2D array
- * @a: 2D array
- */
-
-void print_chessboard(char (*a)[8]);
-
-/**
- * print_diagsums - print sums of diagonals in matrix
- * @a: matrix
- * @size: size of matrix
- */
-
-void print_diagsums(int *a, int size);
-
-/**
- * set_string - set value of pointer to a char
- * @s: variable of type pointer
- * @to: char
- */
-
-void set_string(char **s, char *to);
-
-#endif
+	if (s[i] == c) /* if match, assign to address */
+		return (&s[i]);
+	else
+		return (NULL);
+}

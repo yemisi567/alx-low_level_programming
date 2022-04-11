6e0cba0836e123ca1dbc6a9d33af362e8b7fe098
yemisi567 <51059680+yemisi567@users.noreply.github.com>
2022-04-11 21:58:22 +0100

    fix

--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -1,7 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *c;
+	int *i;
+	float *f;
+	double *d;
 
+	c = malloc_checked(sizeof(char) * 1024);
+	printf("%p\n", (void *)c);
+	i = malloc_checked(sizeof(int) * 402);
+	printf("%p\n", (void *)i);
+	f = malloc_checked(sizeof(float) * 100000000);
+	printf("%p\n", (void *)f);
+	d = malloc_checked(INT_MAX);
+	printf("%p\n", (void *)d);
+	free(c);
+	free(i);
+	free(f);
+	free(d);
+	return (0);
+}
 /**
  * main - check the code for Holberton School students.
  *
@@ -59,5 +86,122 @@ int main(void)
 	a[97] = '!';
 	simple_print_buffer(a, 98);
 	free(a);
+	return (0);
+}
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int *a;
+
+	a = array_range(0, 10);
+	simple_print_buffer(a, 11);
+	free(a);
+	return (0);
+}
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *p;
+	int i;
+
+	p = malloc(sizeof(char) * 10);
+	p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
+	i = 0;
+	while (i < 98)
+	{
+		p[i++] = 98;
+	}
+	simple_print_buffer(p, 98);
+	free(p);
+	return (0);
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *c;
+	int *i;
+	float *f;
+	double *d;
+
+	c = malloc_checked(sizeof(char) * 1024);
+	printf("%p\n", (void *)c);
+	i = malloc_checked(sizeof(int) * 402);
+	printf("%p\n", (void *)i);
+	f = malloc_checked(sizeof(float) * 100000000);
+	printf("%p\n", (void *)f);
+	d = malloc_checked(INT_MAX);
+	printf("%p\n", (void *)d);
+	free(c);
+	free(i);
+	free(f);
+	free(d);
 	return (0);
 }
---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,66 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+#include "4-sum.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    float a;
+    float r;
+
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
+    return (0);
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+    int j;
+
+    i = ABS(-98) * 10;
+    j = ABS(98) * 10;
+    printf("%d, %d\n", i, j);
+    return (0);
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = SUM(98, 1024);
+    printf("%d\n", s);
+    return (0);
+}
